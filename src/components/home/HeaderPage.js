cb0a1f2a0af16dc39cfd6509e204b7a11d7ff6cd
feri.ramdani007 <feri.ramdani007@gmail.com>
2023-09-07 23:13:24 +0700

    fix time

--- src/components/home/HeaderPage.js
+++ src/components/home/HeaderPage.js
@@ -3,57 +3,35 @@ import axios from 'axios'
 import heroImg from './../../assets/image/hero-img-2.png'
 
 const HeaderPage = () => {
-    const [dataJadwalSholat, setDataJadwalSholat] = useState({});
-    const [jadwalSholat, setJadwalSholat] = useState('');
-    const [waktuSholat, setWaktuSholat] = useState('');
-    const [waktuSekarang, setWaktuSekarang] = useState('00:00')
-    const now = new Date();
-    const currentHour = now.getHours();
-    const currentMinute = now.getMinutes();
+    // const [dataJadwalSholat, setDataJadwalSholat] = useState({});
+    // const [jadwalSholat, setJadwalSholat] = useState('');
+    // const [waktuSholat, setWaktuSholat] = useState('');
+    const [waktuSekarang, setWaktuSekarang] = useState('00:00');
+
+    const getTime = async () => {
+        const now = new Date();
+        const currentHour = now.getHours();
+        const currentMinute = now.getMinutes();
+
+        return {currentHour: currentHour, currentMinute: currentMinute}
+    }
 
     useEffect(() => {
-        // Menggabungkan jam dan menit saat ini menjadi format waktu yang dapat dibandingkan (misalnya, "10:30")
-        const currentTime = `${currentHour}:${currentMinute.toString().padStart(2, "0")}`;
-
-        setWaktuSekarang(currentTime)
-
+        getTime().then((res) => {
+            const currentTime = `${res.currentHour}:${res.currentMinute.toString().padStart(2, "0")}`;
+            setWaktuSekarang(currentTime)
+        })
     }, [])
 
     useEffect(() => {
         setInterval(() => {
-            const NewCurrentTime = `${currentHour}:${currentMinute.toString().padStart(2, "0")}`;
-            console.log('NewCurrentTime', NewCurrentTime)
-            setWaktuSekarang(NewCurrentTime)
-        }, 10000);
+            getTime().then((res) => {
+                const newTime = `${res.currentHour}:${res.currentMinute.toString().padStart(2, "0")}`;
+                setWaktuSekarang(newTime)
+            })
+        }, 5000);
     }, [])
-
-    // useEffect(() => {
-    //     // Daftar sholat dalam urutan waktu
-    //     const jadwalSholat = ["subuh", "dzuhur", "ashar", "maghrib", "isya"];
-
-    //     // Mencari jadwal sholat yang akan datang
-    //     let jadwal = dataJadwalSholat;
-    //     let jadwalBerikutnya = null;
-    //     for (let i = 0; i < jadwalSholat.length; i++) {
-    //         const jadwalKey = jadwalSholat[i];
-    //         const jadwalWaktu = jadwal[jadwalKey];
-
-    //         if (waktuSekarang < jadwalWaktu) {
-    //             jadwalBerikutnya = jadwalKey;
-    //             break;
-    //         }
-    //     }
-
-    //     if (jadwalBerikutnya) {
-    //         setJadwalSholat(jadwalBerikutnya)
-    //         setWaktuSholat(jadwal[jadwalBerikutnya])
-    //         console.log(`Jadwal sholat berikutnya adalah ${jadwalBerikutnya} pada pukul ${jadwal[jadwalBerikutnya]}`);
-    //       } else {
-    //         setJadwalSholat('Sedang Gangguan')
-    //         console.log("Tidak ada jadwal sholat yang akan datang hari ini.");
-    //       }
-    // }, [dataJadwalSholat, waktuSekarang])
-
+      
     useEffect(() => {
         getJadwalSholat()
     }, [])
@@ -65,7 +43,7 @@ const HeaderPage = () => {
             const response = await axios.get(`https://api.myquran.com/v1/sholat/jadwal/1219/2021/06/23`)
             console.log('response-kota', response)
             
-            setDataJadwalSholat(response.data.data.jadwal)
+            // setDataJadwalSholat(response.data.data.jadwal)
         } catch (error) {
             
         }
@@ -78,8 +56,8 @@ const HeaderPage = () => {
                     <h1 className="text-slate-200 font-bold tracking-widest">Kota Bandung</h1>
 
                     <div className="text-white bg-emerald-400 bg-opacity-40 backdrop-blur-sm rounded-md px-7 py-3 font-bold tracking-widest mt-4">
-                        <span className='capitalize'>{jadwalSholat}</span>
-                        <span> {waktuSholat}</span>
+                        <span className='capitalize'>{'Subuh'}</span>
+                        <span> {'04:37'}</span>
                     </div>
                 </div>
 
